0f41e1fd3b282497a5321d830e8ce8a4087536b6
Hollis Akins <hollis.akins@gmail.com>
2024-08-14 09:31:33 -0500

    update gitignore

--- brisket/models/accretion_disk_model.py
+++ brisket/models/accretion_disk_model.py
@@ -14,18 +14,19 @@ class accretion_disk(object):
     wavelengths : np.ndarray
         1D array of wavelength values desired for the stellar models.
     
-    parameters : dict
-        dictionary or parameters for AGN accretion disk module.
+    model_comp : dict
+        Subset of the model_components dictionary with AGN accretion disk parameters
     """
 
-    def __init__(self, wavelengths, parameters, logger=utils.NullLogger):
+    def __init__(self, wavelengths, model_comp, logger=utils.NullLogger):
         self.wavelengths = wavelengths
-        self.type = parameters['type']
+        self.type = model_comp['type']
+        # could be optimized more
 
-    def spectrum(self, parameters):
+    def spectrum(self, model_comp):
         if self.type=='plaw':
-            beta = parameters['beta']
-            Muv = parameters['Muv'] # absolute magnitude
+            beta = model_comp['beta']
+            Muv = model_comp['Muv'] # absolute magnitude
             fnu_uv = np.power(10., -(Muv+48.60)/2.5) * u.erg/u.s/u.Hz/u.cm**2
             Snu_uv = (fnu_uv * 4*np.pi*(10*u.pc)**2).to(u.erg/u.s/u.Hz)
             Slam_uv = (Snu_uv * c/(1500*u.angstrom)**2).to(u.Lsun/u.angstrom).value # in Lsun/angstrom
@@ -41,9 +42,9 @@ class accretion_disk(object):
             """Set multi-powerlaw continuum in flux density per unit frequency."""
             # Flip signs of powerlaw slopes to enable calculation to be performed
             # as a function of wavelength rather than frequency
-            sl1 = parameters['beta1']
-            sl2 = parameters['beta2']
-            wavbrk1 = parameters['wav1']
+            sl1 = model_comp['beta1']
+            sl2 = model_comp['beta2']
+            wavbrk1 = model_comp['wav1']
             flxnrm=1.0
             wavnrm=5500
             # Define normalisation constant to ensure continuity at wavbrk
@@ -67,7 +68,7 @@ class accretion_disk(object):
                 fluxtemp)
 
 
-            Muv = parameters['Muv'] # absolute magnitude
+            Muv = model_comp['Muv'] # absolute magnitude
             fnu_uv = np.power(10., -(Muv+48.60)/2.5)
             Snu_uv = (fnu_uv * 4*np.pi*(3.086e19)**2) # in erg/s/Hz
             Slam_uv = (Snu_uv * c/(1500)**2)/3.846e33 # in Lsun/A
@@ -87,7 +88,7 @@ class accretion_disk(object):
             tophat = np.where((self.wavelengths > 1450)&(self.wavelengths < 1550), 1, 0)
             y0 = np.trapz(y*tophat, x=self.wavelengths)
 
-            Muv = parameters['Muv'] # absolute magnitude
+            Muv = model_comp['Muv'] # absolute magnitude
             fnu_uv = np.power(10., -(Muv+48.60)/2.5) * u.erg/u.s/u.Hz/u.cm**2
             Snu_uv = (fnu_uv * 4*np.pi*(10*u.pc)**2).to(u.erg/u.s/u.Hz)
             Slam_uv = (Snu_uv * c/(1500*u.angstrom)**2).to(u.Lsun/u.angstrom).value # in Lsun/angstrom
--- brisket/models/dust_emission_model.py
+++ brisket/models/dust_emission_model.py
@@ -7,33 +7,52 @@ from brisket import utils
 
 
 class dust_emission(object):
-    """ Allows access to and maniuplation of the dust emission models
-    of Draine + Li (2007). Currently very simple, possibly could be sped
-    up in some circumstances by pre-interpolating to the input wavs.
+    """ Allows access to dust emission models. Currently implemented models: 
+
+        - Draine & Li (2007) radiative transfer models
+        - Drew & Casey (2021) modified blackbody + mid-IR power law 
 
     Parameters
     ----------
 
     wavelengths : np.ndarray
-        1D array of wavelength values desired for the DL07 models.
+        1D array of wavelength values desired for the dust emission models.
+    
+    redshift : float
+        redshift of the model
+    
+    model_comp : dict
+        Subset of the model_components dictionary with dust emission parameters
     """
 
-    def __init__(self, wavelengths, params, logger=utils.NullLogger):
+    def __init__(self, wavelengths, redshift, model_comp):
         self.wavelengths = wavelengths
-        self.params = params
-        self.logger = logger
+        self.redshift = redshift
+        self.model_comp = model_comp
+        if self.model_comp['type'] == 'DL07':
+            self.spectrum = self.spectrum_DL07
+        elif self.model_comp['type'] == 'MBBPL':
+            self.spectrum = self.spectrum_MBBPL
 
-        self.flag = True
-        if not ('dust_atten' in list(params) and 'dust_emission' in list(params)):
-            self.logger.info("Skipping dust emission module")
-            self.flag = False; return
-        
-        self.logger.info(f"Initializing dust emission module".ljust(50) + f'()'.rjust(20))
 
-    def spectrum(self, params):
-        """ Get the 1D spectrum for a given set of model parameters. """
+    def spectrum_DL07(self, model_comp):
+        """ Get the Draine & Li (2007) model for a given set of model 
+        parameters, given as keys in model_comp.
 
-        qpah, umin, gamma = params['dust_emission']['qpah'], params['dust_emission']['umin'], params['dust_emission']['gamma']
+        Parameters
+        ----------
+
+        qpah : float
+            The PAH fraction
+        umin : float
+            The minimum of the starlight intensity distribution
+        gamma : float 
+            The fraction of the dust heated by starlight (0-1)
+        """
+
+        qpah = model_comp['qpah']
+        umin = model_comp['umin']
+        gamma = model_comp['gamma']
 
         qpah_ind = config.qpah_vals[config.qpah_vals < qpah].shape[0]
         umin_ind = config.umin_vals[config.umin_vals < umin].shape[0]
@@ -48,13 +67,13 @@ class dust_emission(object):
 
         umin_w = np.array([(1 - umin_fact), umin_fact])
 
-        lqpah_only = config.dust_grid_umin_only[qpah_ind].data
-        hqpah_only = config.dust_grid_umin_only[qpah_ind+1].data
+        lqpah_only = config.dust_grid_umin_only[qpah_ind]
+        hqpah_only = config.dust_grid_umin_only[qpah_ind+1]
         tqpah_only = (qpah_fact*hqpah_only[:, umin_ind:umin_ind+2]
                       + (1-qpah_fact)*lqpah_only[:, umin_ind:umin_ind+2])
 
-        lqpah_umax = config.dust_grid_umin_umax[qpah_ind].data
-        hqpah_umax = config.dust_grid_umin_umax[qpah_ind+1].data
+        lqpah_umax = config.dust_grid_umin_umax[qpah_ind]
+        hqpah_umax = config.dust_grid_umin_umax[qpah_ind+1]
         tqpah_umax = (qpah_fact*hqpah_umax[:, umin_ind:umin_ind+2]
                       + (1-qpah_fact)*lqpah_umax[:, umin_ind:umin_ind+2])
 
@@ -64,10 +83,72 @@ class dust_emission(object):
         model = gamma*interp_umax + (1 - gamma)*interp_only
 
         spectrum = np.interp(self.wavelengths,
-                             config.dust_grid_umin_only[1].data[:, 0],
+                             config.dust_grid_umin_only[1][:, 0],
                              model, left=0., right=0.)
 
         return spectrum
 
-    def __bool__(self):
-        return self.flag
+
+    def spectrum_MBBPL(self, model_comp):
+        """ Compute the Drew & Casey (2021) model for a given 
+        set of model parameters, given as keys in model_comp.
+
+        Parameters
+        ----------
+
+        Tdust : float
+            The dust temperature 
+        beta : float
+            ...
+        alpha : float 
+            ...
+        lam0 : float 
+            ...
+        """
+
+        T = model_comp['Tdust']
+        beta = model_comp['beta']
+        alpha = model_comp['alpha']
+        lam0 = model_comp['lam0'] # in micron
+
+        hck = 143877687.75039333 # Kelvin*angstrom
+        c = 2.9979e18 # angstrom/s
+    
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            z = self.redshift
+            T_CMB_z0 = 2.73
+            T_CMB = T_CMB_z0*(1+z)
+            Tdust_z = (T**(beta+4) + T_CMB_z0**(beta+4) * ((1+z)**(beta+4)-1))**(1/(beta+4))
+            CMB = 1 - (np.power(c/self.wavelengths, 3) / (np.exp(hck/(self.wavelengths*T_CMB))-1)) / (np.power(c/self.wavelengths, 3) / (np.exp(hck/(self.wavelengths*Tdust_z))-1))
+            T = Tdust_z
+            CMB = np.where(self.wavelengths < 1e4, 1, CMB)
+
+            lam_fine = np.logspace(3, 6.5, 1000)
+            if lam0 == 'ot': MBB = np.power(c/lam_fine, beta) * np.power(c/lam_fine, 3) / (np.exp(hck/(lam_fine*T))-1) # optically thin 
+            else: MBB = (1-np.exp(-(lam0*1e4/lam_fine)**beta)) * np.power(c/lam_fine, 3) / (np.exp(hck/(lam_fine*T))-1) # general opacity
+            delta_y = np.diff(np.log10(MBB))
+            delta_x = np.diff(np.log10(lam_fine))
+            deriv = delta_y / delta_x
+            
+            lam_fine = 0.5*(lam_fine[1:] + lam_fine[:-1])
+            lam_int = lam_fine[np.nanargmin(np.abs(deriv-alpha))]
+            
+            Npl = MBB[np.argmin(np.abs(lam_fine-lam_int))] * lam_int**(-alpha)
+            PL = Npl * self.wavelengths**alpha
+
+            if lam0 == 'ot': MBB = np.power(c/self.wavelengths, beta) * np.power(c/self.wavelengths, 3) / (np.exp(hck/(self.wavelengths*T))-1) # optically thin 
+            else: MBB = (1-np.exp(-(lam0*1e4/self.wavelengths)**beta)) * np.power(c/self.wavelengths, 3) / (np.exp(hck/(self.wavelengths*T))-1) # general opacity
+            spectrum =  np.where(self.wavelengths < lam_int, PL, MBB)
+            # spectrum = np.where(self.wavelengths < 1.6e4, 0, spectrum)
+
+            # convert from F_nu to F_lambda
+            spectrum = spectrum / self.wavelengths**2
+            # spectrum *= np.power(10., 1/(1+np.exp(-20*(np.log10(self.wavelengths)-4.8))))
+        
+
+            spectrum = np.power(10., np.log10(spectrum) * 1/(1+np.exp(-20*(np.log10(self.wavelengths)-3.8))))
+            # renormalize to 1
+            spectrum = spectrum / np.trapz(spectrum, x=self.wavelengths)
+
+        return spectrum * CMB
