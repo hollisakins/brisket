eaacc8b00c64d295469212f07014ba7ef8768a91
Hollis Akins <hollis.akins@gmail.com>
2024-08-28 16:25:52 -0500

    parameters 3

--- brisket/tests/test_plot.py
+++ brisket/tests/test_plot.py
@@ -5,51 +5,33 @@ import numpy as np
 plt.style.use('hba_sans')
 
 import brisket
-p = brisket.parameters.ParamDict('alpha')
-print(p)
-
-
-# p = {}
-# p['redshift'] = {'low':6, 'high':8, 'prior': 'Gaussian', 'mu':5, 'sigma':3}
-# p['galaxy'] = {'stellar_model':'BC03', 'logMstar': {'low':10, 'high':11}}
-# p['galaxy']['nebular'] = {'fesc':0, 'logU':{'low':-4,'high':-1}}
-# p['redshift'] = 7
-# p['galaxy'] = {'stellar_model':'BC03', 'logMstar': 10}
-# p['galaxy']['nebular'] = {'fesc':0, 'logU':-4}
-
-quit()
-
-p = {'redshift': 7}
-p['igm'] = 'Inoue14'
-p['galaxy'] = {
-    'stellar_model': 'BC03',
-    'logMstar': {'low':10, 'high':11, 'prior':'Gaussian', 'mu':10.5, 'sigma':0.2},
-    'metallicity': {'low':0.01, 'high':2.5, 'prior':'log_10'}, 
-    'sfh': 'constant',
-    'age_min': 0,
-    'age_max': 1,
-}
-p['galaxy']['nebular'] = {
-    'type':'flex',
-    'Hb': {'low':1e-21, 'high':1e-19},
-    'OIII5007': {'low':1e-21, 'high':1e-18},
-    'OIII4959': {'mirror':'OIII5007', 'transform': lambda x: x/3},
-    'Ha': {'mirror':'Hb'}
-}
-p = brisket.parameters.ParamDict(p)
-
-
-# p['calibration'] = {'template':'prism'}
-
-
+p = brisket.parameters.Params('basic')
 ### OR 
-p = brisket.parameters.load_from_toml('tests/param_fit_test.toml')
+# p = brisket.parameters.Params('tests/param_fit_test.toml')
 
-gal = brisket.model_galaxy(p, filt_list=['f115w','f150w','f277w','f444w'])
-
-
+# gal = brisket.ModelGalaxy(p, filt_list=['f115w','f150w','f277w','f444w'], logger=brisket.utils.basicLogger)
 
 
+
+def load_phot(ID):
+    flux = 3631e6*np.power(10., -0.4*np.array([23.8, 23.9, 23.5, 22.9]))
+    err = 0.1*flux
+    return np.array([flux, err]).T
+
+# p = {'redshift': 7}
+# p['galaxy'] = {
+#     'stellar_model': 'BC03',
+#     'logMstar': {'low':10,'high':12},
+#     'metallicity': {'low':0.001, 'high':1.0, 'prior':'log_10'},
+#     'sfh': 'constant',
+#     'age_min': 0,
+#     'age_max': 1,
+# }
+# p = brisket.parameters.Params(p)
+
+gal = brisket.Galaxy(1, load_phot=load_phot, filt_list=['f115w','f150w','f277w','f444w'], logger=brisket.utils.basicLogger)
+fitter = brisket.Fitter(gal, p, run='test828', logger=brisket.utils.basicLogger)
+fitter.fit(verbose=True, n_live=100, sampler='multinest')
 
 
 fig, ax = plt.subplots()
@@ -61,24 +43,33 @@ fig, ax = plt.subplots()
 #         'dust_atten':{'type':'Calzetti','Av':3,'logfscat':-2}}, 
 #       'redshift': 7}
 # gal = brisket.model_galaxy(mc, filt_list=['f115w','f150w','f277w','f444w'])
-# ax.semilogx(gal.wav_obs, -2.5*np.log10(gal.spectrum_full/3631e6))
+
+
+f = fits.getdata('brisket/posterior/test828/1_brisket_results.fits', extname='SED_MED')
+ax.semilogx(f['wav_rest']*8, -2.5*np.log10(f['f_lam_50']/3631e6))
+f = fits.getdata('brisket/posterior/test828/1_brisket_results.fits', extname='PHOT')
+print(f['flux'])
+ax.scatter(f['wav']/1e4, -2.5*np.log10(f['flux']/3631e6))
+
+# ax.semilogx(gal.wav_obs, -2.5*np.log10(gal.sed/3631e6))
 
 # mc['redshift'] = 5.5
 # mc['galaxy']['dust_atten']['Av'] = 1.5
 # gal.update(mc)
 # ax.semilogx(gal.wav_obs, -2.5*np.log10(gal.spectrum_full/3631e6))
-f = fits.getdata('brisket/tests/brisket/posterior/test7/756434_brisket_results.fits', extname='SED_MED')
-ax.loglog(f['wav_rest']*8, f['f_lam_50'])
+# f = fits.getdata('brisket/tests/brisket/posterior/test7/756434_brisket_results.fits', extname='SED_MED')
+# ax.loglog(f['wav_rest']*8, f['f_lam_50'])
 
-f = fits.getdata('brisket/tests/brisket/posterior/test10/756434_brisket_results.fits', extname='SED_MED')
-ax.loglog(f['wav_rest']*8, f['f_lam_50'])
+# f = fits.getdata('brisket/tests/brisket/posterior/test10/756434_brisket_results.fits', extname='SED_MED')
+# ax.loglog(f['wav_rest']*8, f['f_lam_50'])
 
-f = fits.getdata('brisket/tests/brisket/posterior/test7/756434_brisket_results.fits', extname='PHOT')
-print(f['filter'])
-ax.errorbar(f['wav']/1e4, f['flux'], yerr=f['flux_err'], linewidth=0, elinewidth=1, marker='o')
+# f = fits.getdata('brisket/tests/brisket/posterior/test7/756434_brisket_results.fits', extname='PHOT')
+# print(f['filter'])
+# ax.errorbar(f['wav']/1e4, f['flux'], yerr=f['flux_err'], linewidth=0, elinewidth=1, marker='o')
 
 # ax.semilogx(f['wav']/1e4, -2.5*np.log10(gal.photometry/3631e6))
 
-ax.set_xlim(0.7, 10)
-ax.set_ylim(7e-3, 2)
+ax.set_xlim(0.5, 10)
+# ax.set_ylim(7e-3, 2)
+ax.set_ylim(28, 20)
 plt.show()
