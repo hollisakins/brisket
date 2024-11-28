d120463fefa731d90e192ea8ebbfe8137277d8fa
Hollis Akins <hollis@Holliss-MacBook-Pro.local>
2024-11-27 22:42:31 -0500

    docs

--- brisket/fitting/__init__.py
+++ brisket/fitting/__init__.py
@@ -1,7 +1,11 @@
 from __future__ import print_function, division, absolute_import
 
-# from brisket.fitting.fitter import Fitter
-# from brisket.fitting.fitted_model import FittedModel
-from brisket.fitting.priors import Prior, PriorVolume
-# from brisket.fitting.posterior import Posterior
-# from brisket.fitting.check_priors import check_priors
+from . import data
+from . import fitted_model
+from . import check_priors
+from . import fitter
+from . import noise
+from . import posterior
+from . import priors
+
+from priors import Prior, PriorVolume
