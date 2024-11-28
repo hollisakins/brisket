d7e4ea43b6bbe0e2a799717956c0cb44bcf01252
Hollis Akins <hollis@Holliss-MacBook-Pro.local>
2024-11-27 22:00:30 -0500

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -- Path
 import sys, os
-sys.path.insert(0, os.path.abspath("../../brisket"))
-# sys.path.insert(0, os.path.abspath("../../"))
+# sys.path.insert(0, os.path.abspath("../../brisket"))
+sys.path.insert(0, os.path.abspath("../../"))
 # sys.path.insert(0, os.path.abspath("../brisket")) 
 # sys.path.insert(0, os.path.abspath("../"))
 
